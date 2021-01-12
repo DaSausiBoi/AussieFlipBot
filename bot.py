a1334f981f0c61368c1ba9e76623ef9c061f4789
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2021-01-11 22:32:58 -0800

    added 'shower_thought', wip

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN') #check .env for token
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!') #figure out how to make this changeable through discord commands
 
-@bot.command(name='quote', help='Responds with a random quote.')
+@bot.command(name='quote', help='Responds with a random quote.') #idfk make more quotes
 async def quoted(ctx):
     quotes = [
         'Bruh moment',
@@ -20,7 +20,24 @@ async def quoted(ctx):
     response = random.choice(quotes)
     await ctx.send(response)
 
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
+@bot.command(name='shower_thought', help='Gives you a shower thought to think about.') #r/showerthoughts
+async def showerThought(ctx):
+    showerthoughts = [
+        '85 is divisible by 17.',
+        'Commercials on your phone are objectively worse than those on the TV, because you can\'t distract yourself with your phone while they run.',
+        'In most situations, when you buy a lottery ticket, you are paying for a slip of paper that says \"You\'re a loser.\"',
+        'The ocean might be a little bit deeper if sponges weren\'t living in them.',
+        'The Soviet National Anthem isn\'t copyrighted, so it technically is \"our national anthem.\"',
+        'The height 5\' 8\" could also be written as 4\' 20\".',
+        'Turning the lights off when you are alone always seems to make things more quiet.',
+        'Being able to tolerate the sound of your own voice in a video is probably the highest form of self acceptance.',
+        'During a nuclear explosion, there is a certain radius where all the frozen supermarket pizzas are cooked to perfection.',
+    ]
+
+    thought = random.choice(showerthoughts)
+    await ctx.send(thought)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.') #peepeepoopoo roll dice caveman style
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
@@ -28,13 +45,13 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
-@bot.command(name='alpha_test', help='Test command for the \"Alpha\" role.')
+@bot.command(name='alpha_test', help='Test command for the \"Alpha\" role.') #test command for those with 'alpha' role
 @commands.has_role('Alpha')
 async def alpha_test(ctx):
     alphaResponse = 'Test clear, perms are set properly.'
     await ctx.send(alphaResponse)
 
-@bot.event
+@bot.event #makes sure user has 'alpha' role
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the right role for this command, please check and make sure all perms are set properly.')
