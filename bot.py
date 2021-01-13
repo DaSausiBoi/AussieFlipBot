0a51d03d3c5257ae28ab9f82a9e58d14bba4c033
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2021-01-13 15:03:44 -0800

    changed shower thoughts format, added thoughts doc, minor spelling

--- bot.py
+++ bot.py
@@ -23,6 +23,10 @@ async def quoted(ctx):
     response = random.choice(quotes)
     await ctx.send(response)
 
+@bot.command(name='ping', help='Responds with the response time of the bot.') #answeres with ms latency
+async def ping(ctx):
+    await ctx.send(f'Pong! {round (bot.latency * 1000)} ms ')
+
 #@bot.command(name='prefix_change', help='Changes bot prefix.') #fix this shit aint working
 #async def prefixSet(ctx, prefix: str):
     #prefixChange(prefix)
@@ -30,32 +34,9 @@ async def quoted(ctx):
 
 @bot.command(name='thought', help='Gives you a shower thought to think about.') #r/showerthoughts
 async def showerThought(ctx):
-    showerthoughts = [
-        '85 is divisible by 17.',
-        'Commercials on your phone are objectively worse than those on the TV, because you can\'t distract yourself with your phone while they run.',
-        'In most situations, when you buy a lottery ticket, you are paying for a slip of paper that says \"You\'re a loser.\"',
-        'The ocean might be a little bit deeper if sponges weren\'t living in them.',
-        'The Soviet National Anthem isn\'t copyrighted, so it technically is \"our national anthem.\"',
-        'The height 5\' 8\" could also be written as 4\' 20\".',
-        'Turning the lights off when you are alone always seems to make things more quiet.',
-        'Being able to tolerate the sound of your own voice in a video is probably the highest form of self acceptance.',
-        'During a nuclear explosion, there is a certain radius where all the frozen supermarket pizzas are cooked to perfection.',
-        'In rap music, you are more respected the larger your city is. In country music, you are more respected the smaller your town is.',
-        'People are biodegradeable, but also terrible for the environment.',
-        'The Amish are talked about on the internet, but they are completely unaware of it.',
-        'The shin can be used as a tool to help find furniture in the dark.',
-        'The children’s book \"If You Give a Mouse a Cookie\" is really about the gateways of addiction.'
-        'We\'re all a little shit in the inside, literally.',
-        'The microwave beeping is only annoying when you don\'t need it.',
-        'Adding \"the\" to \"shit\" makes something described as bad turn into good.',
-        'It\'s impossible to blink faster than you normally do.',
-        'Gunpowder is spicy sand.',
-        'Future generations studying history will likely have to study tweets.',
-        'Cavemen probably laughed at farts before they could speak.', 
-        'You learn to read, then you read to learn, but if you can’t read then you can’t learn, and if you can’t speak English, English is like an alien language.'
-    ]
-
-    thought = random.choice(showerthoughts)
+    thoughts = open('thoughts.txt').read().splitlines()
+    random.seed(a=None)
+    thought = random.choice(thoughts)
     await ctx.send(thought)
 
 @bot.command(name='dice', help='Simulates rolling dice.') #peepeepoopoo roll dice gangnam style
