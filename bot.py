283fb93200201c1166b58452e54e97491593578c
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2021-01-13 15:19:06 -0800

    added help embed, added bot reacting to 'bruh', added loading failsafe

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
+print('Bot starting up...')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') #check .env for token
@@ -11,14 +12,14 @@ TOKEN = os.getenv('DISCORD_TOKEN') #check .env for token
 command_prefix = '?'
 bot = commands.Bot(command_prefix) #figure out how to make this changeable through discord commands
 
-@bot.command(name='quote', help='Responds with a random quote.') #idfk make more quotes
+@bot.command #idfk make more quotes
 async def quoted(ctx):
     quotes = open('quote.txt').read().splitlines()
     random.seed(a=None)
     quote = random.choice(quotes)
     await ctx.send(quote)
 
-@bot.command(name='ping', help='Responds with the response time of the bot.') #answeres with ms latency
+@bot.command #answeres with ms latency
 async def ping(ctx):
     await ctx.send(f'Pong! {round (bot.latency * 1000)} ms ')
 
@@ -27,14 +28,14 @@ async def ping(ctx):
     #prefixChange(prefix)
     #await ctx.send('Prefix set!')
 
-@bot.command(name='thought', help='Gives you a shower thought to think about.') #r/showerthoughts
+@bot.command #r/showerthoughts
 async def showerThought(ctx):
     thoughts = open('thoughts.txt').read().splitlines()
     random.seed(a=None)
     thought = random.choice(thoughts)
     await ctx.send(thought)
 
-@bot.command(name='dice', help='Simulates rolling dice.') #peepeepoopoo roll dice gangnam style
+@bot.command #peepeepoopoo roll dice gangnam style
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
@@ -42,7 +43,7 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
-@bot.command(name='alpha_test', help='Test command for the \"Alpha\" role.') #test command for those with 'alpha' role
+@bot.command #test command for those with 'alpha' role
 @commands.has_role('Alpha')
 async def alpha_test(ctx):
     alphaResponse = 'Test clear, perms are set properly.'
@@ -53,6 +54,24 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the right permissions for this command.')
 
+client.remove_command(‘help’) #We delete default help command
+@client.command(pass_context=True) #Embeded help with list and details of commands
+async def help(ctx):
+    embed = discord.Embed(
+        colour = discord.Colour.blue())
+    embed.set_author(name='List of commands available')
+    embed.add_field(name='?ping', value='Returns bot respond time in milliseconds.', inline=False)
+    embed.add_field(name='?dice', value='Simulates rolling dice.', inline=False)
+    embed.add_field(name='?thought', value='Gives you a shower thought to think about.', inline=False)
+    embed.add_field(name='?quote', value='Get inspired by a powerful quote.', inline=False)
+    embed.add_field(name='?alpha_test', value='Test command for the \"Alpha\" role.', inline=False)
+    await ctx.send(embed=embed)
+
+@client.event #react to any message
+async def on_message(ctx):
+    if 'bruh' in ctx.content:
+        emoji = '\N{EGGPLANT}'
+        await ctx.add_reaction(emoji)
 
 @bot.event #sets bot status
 async def on_ready():
