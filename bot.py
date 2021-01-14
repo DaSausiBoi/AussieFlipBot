97fd1d40876f8bbd3b95c5c44e84c7c508a2c79e
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2021-01-13 18:34:04 -0800

    fixed help embed, broke everything else, working on fix

--- bot.py
+++ bot.py
@@ -12,8 +12,10 @@ TOKEN = os.getenv('DISCORD_TOKEN') #check .env for token
 command_prefix = '?'
 bot = commands.Bot(command_prefix) #figure out how to make this changeable through discord commands
 
+bot.remove_command('help') #delete default help command
+
 @bot.command #idfk make more quotes
-async def quoted(ctx):
+async def quote(ctx):
     quotes = open('quote.txt').read().splitlines()
     random.seed(a=None)
     quote = random.choice(quotes)
@@ -29,14 +31,14 @@ async def ping(ctx):
     #await ctx.send('Prefix set!')
 
 @bot.command #r/showerthoughts
-async def showerThought(ctx):
+async def thought(ctx):
     thoughts = open('thoughts.txt').read().splitlines()
     random.seed(a=None)
     thought = random.choice(thoughts)
     await ctx.send(thought)
 
 @bot.command #peepeepoopoo roll dice gangnam style
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
+async def dice(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
@@ -54,12 +56,11 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the right permissions for this command.')
 
-bot.remove_command('help') #We delete default help command
 @bot.command(pass_context=True) #Embeded help with list and details of commands
 async def help(ctx):
     embed = discord.Embed(
-        colour = discord.Colour.blue())
-    embed.set_author(name='List of commands available')
+        colour = discord.Colour.green())
+    embed.set_author(name='Commands List')
     embed.add_field(name='?ping', value='Returns bot respond time in milliseconds.', inline=False)
     embed.add_field(name='?dice', value='Simulates rolling dice.', inline=False)
     embed.add_field(name='?thought', value='Gives you a shower thought to think about.', inline=False)
@@ -67,16 +68,10 @@ async def help(ctx):
     embed.add_field(name='?alpha_test', value='Test command for the \"Alpha\" role.', inline=False)
     await ctx.send(embed=embed)
 
-@bot.event #react to any message
-async def on_message(ctx):
-    if 'bruh' in ctx.content:
-        emoji = '\N{EGGPLANT}'
-        await ctx.add_reaction(emoji)
-
 @bot.event #sets bot status
 async def on_ready():
     activity = discord.Game(name='Infinite Recharge at Home', type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity) #discord.Status.online, discord.Status.idle, discord.Status.do_not_disturb
     print('Bot is ready!')
 
-bot.run(TOKEN) 
+bot.run(TOKEN)
