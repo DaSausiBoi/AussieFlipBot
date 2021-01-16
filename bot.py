b8029929f42bfec567a70a98b26737e45c3e39b1
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2021-01-15 21:18:20 -0800

    changed look of help

--- bot.py
+++ bot.py
@@ -69,14 +69,14 @@ async def on_command_error(ctx, error):
 async def help(ctx):
     embed = discord.Embed(
         colour = discord.Colour.green())
-    embed.set_author(name='Commands List')
-    embed.add_field(name='?flip (text)', value='Flips your text.', inline=False)
-    embed.add_field(name='?ping', value='Returns bot respond time in milliseconds.', inline=False)
-    embed.add_field(name='?spam (text)', value='Spams text 10 times in a row.', inline=False)
-    embed.add_field(name='?dice (# of dice) (highest dice value)', value='Simulates rolling dice.', inline=False)
-    embed.add_field(name='?thought', value='Gives you a shower thought to think about.', inline=False)
-    embed.add_field(name='?quote', value='Get inspired by a powerful quote.', inline=False)
-    embed.add_field(name='?alpha_test', value='Test command for the \"Alpha\" role.', inline=False)
+    embed.set_author(name='Commands List, prefix: ' + command_prefix)
+    embed.add_field(name='flip (text)', value='Flips your text.', inline=False)
+    embed.add_field(name='ping', value='Returns bot respond time in milliseconds.', inline=False)
+    embed.add_field(name='spam (text)', value='Spams text 10 times in a row.', inline=False)
+    embed.add_field(name='dice (# of dice) (highest dice value)', value='Simulates rolling dice.', inline=False)
+    embed.add_field(name='thought', value='Gives you a shower thought to think about.', inline=False)
+    embed.add_field(name='quote', value='Get inspired by a powerful quote.', inline=False)
+    embed.add_field(name='alpha_test', value='Test command for the \"Alpha\" role.', inline=False)
     await ctx.send(embed=embed)
 
 @bot.event #sets bot status
