1f79e1f1adb5448db22ecddd704216b247df33a9
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2021-01-23 00:32:53 -0800

    added blacklist failsafe

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ print('Bot starting up...')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') #check .env for token
 
+blacklist =[617063458466299953]
+
 command_prefix = '?'
 bot = commands.Bot(command_prefix) #figure out how to make this changeable through discord commands
 
@@ -17,56 +19,101 @@ bot.remove_command('help') #delete default help command
 
 @bot.command(pass_context=True) #idfk make more quotes
 async def quote(ctx):
-    quotes = open('quote.txt').read().splitlines()
-    random.seed(a=None)
-    quote = random.choice(quotes)
-    await ctx.send(quote)
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        quotes = open('quote.txt').read().splitlines()
+        random.seed(a=None)
+        quote = random.choice(quotes)
+        await ctx.send(quote)
 
 @bot.command(pass_context=True) #answeres with ms latency
 async def ping(ctx):
-    await ctx.send(f'Pong! {round (bot.latency * 1000)} ms ')
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        await ctx.send(f'Pong! {round (bot.latency * 1000)} ms ')
 
 @bot.command(pass_context=True) #69 nice
 async def sixtynine(ctx):
-    await ctx.send('nice')
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        await ctx.send('nice')
 
 @bot.command(pass_context=True) #i just flipped a switch
 async def flip(ctx, *, fliptext): #parses user input as a string then sends it to flip command
-    textflip = upsidedown.transform(str(fliptext))
-    await ctx.send(textflip)
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        textflip = upsidedown.transform(str(fliptext))
+        await ctx.send(textflip)
 
 @bot.command(pass_context=True) #r/showerthoughts
 async def thonk(ctx):
-    thoughts = open('thoughts.txt').read().splitlines()
-    random.seed(a=None)
-    thought = random.choice(thoughts)
-    await ctx.send(thought)
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        thoughts = open('thoughts.txt').read().splitlines()
+        random.seed(a=None)
+        thought = random.choice(thoughts)
+        await ctx.send(thought)
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #for avery
 async def money(ctx):
-    await ctx.send('I FEEL SO CLEAN LIKE A MONEY MACHINE')
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        await ctx.send('I FEEL SO CLEAN LIKE A MONEY MACHINE')
 
 @bot.command(pass_context=True) #peepeepoopoo roll dice gangnam style
 async def dice(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        dice = [
+            str(random.choice(range(1, number_of_sides + 1)))
+            for _ in range(number_of_dice)
+        ]
+        await ctx.send(', '.join(dice))
 
 @bot.command(pass_context=True) #test command for those with 'alpha' role
 @commands.has_role('Alpha')
 async def alpha_test(ctx):
-    alphaResponse = 'Test clear, perms are set properly.'
-    await ctx.send(alphaResponse)
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        alphaResponse = 'Test clear, perms are set properly.'
+        await ctx.send(alphaResponse)
 
 @bot.command(pass_context=True) #spam musubi
 async def spam(ctx, *, cum):
-    pingNum = int(10)
-    while pingNum > 0:
-        alphaResponse = cum 
-        await ctx.send(alphaResponse)
-        pingNum = pingNum - 1
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        pingNum = int(10)
+        while pingNum > 0:
+            alphaResponse = cum 
+            await ctx.send(alphaResponse)
+            pingNum = pingNum - 1
     
 @bot.event #makes sure user has 'alpha' role
 async def on_command_error(ctx, error):
@@ -75,19 +122,24 @@ async def on_command_error(ctx, error):
 
 @bot.command(pass_context=True) #Embeded help with list and details of commands
 async def help(ctx):
-    embed = discord.Embed(
-        colour = discord.Colour.green())
-    embed.set_author(name='Commands List, prefix: ' + command_prefix)
-    embed.add_field(name='flip (text)', value='Flips your text.', inline=False)
-    embed.add_field(name='ping', value='Returns bot respond time in milliseconds.', inline=False)
-    embed.add_field(name='spam (text)', value='Spams text 10 times in a row.', inline=False)
-    embed.add_field(name='dice (# of dice) (highest dice value)', value='Simulates rolling dice.', inline=False)
-    embed.add_field(name='thonk', value='Gives you a shower thought to think about.', inline=False)
-    embed.add_field(name='quote', value='Get inspired by a powerful quote.', inline=False)
-    embed.add_field(name='sixtynine', value='nice', inline=False)
-    embed.add_field(name='money', value='I FEEL SO CLEAN LIKE A MONEY MACHINE', inline=False)
-    embed.add_field(name='alpha_test', value='Test command for the \"Alpha\" role.', inline=False)
-    await ctx.send(embed=embed)
+    if ctx.author.id in blacklist:
+        await ctx.send('You are blacklisted.')
+        await ctx.message.delete()
+        return
+    else:
+        embed = discord.Embed(
+            colour = discord.Colour.green())
+        embed.set_author(name='Commands List, prefix: ' + command_prefix)
+        embed.add_field(name='flip (text)', value='Flips your text.', inline=False)
+        embed.add_field(name='ping', value='Returns bot respond time in milliseconds.', inline=False)
+        embed.add_field(name='spam (text)', value='Spams text 10 times in a row.', inline=False)
+        embed.add_field(name='dice (# of dice) (highest dice value)', value='Simulates rolling dice.', inline=False)
+        embed.add_field(name='thonk', value='Gives you a shower thought to think about.', inline=False)
+        embed.add_field(name='quote', value='Get inspired by a powerful quote.', inline=False)
+        embed.add_field(name='sixtynine', value='nice', inline=False)
+        embed.add_field(name='money', value='I FEEL SO CLEAN LIKE A MONEY MACHINE', inline=False)
+        embed.add_field(name='alpha_test', value='Test command for the \"Alpha\" role.', inline=False)
+        await ctx.send(embed=embed)
 
 @bot.event #sets bot status
 async def on_ready():
